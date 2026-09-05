43025e38962715e25cd6da45e62a63b2ed144837
Aliyugambo <gamboaliyu00@gmail.com>
2026-09-05 20:51:49 +0100

    ADD:CODES

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -44,7 +44,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  const port = process.env.API_PORT || 3001;
+  const port = process.env.PORT || process.env.API_PORT || 3001;
   await app.listen(port);
   console.log(`Application is running on port ${port}`);
 }
